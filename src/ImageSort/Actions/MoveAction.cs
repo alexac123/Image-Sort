78d34d1b8915fc3b5b07daa16644cf60d36c50a3
alexac123 <65075143+alexac123@users.noreply.github.com>
2023-02-17 13:24:26 +0000

    Rename when moved

--- src/ImageSort/Actions/MoveAction.cs
+++ src/ImageSort/Actions/MoveAction.cs
@@ -1,58 +1,62 @@
-﻿using System;
+using System;
 using System.IO;
 using ImageSort.FileSystem;
 using ImageSort.Localization;
 
-namespace ImageSort.Actions;
-
-public class MoveAction : IReversibleAction
+namespace ImageSort.Actions
 {
-    private readonly IFileSystem fileSystem;
-    private readonly string oldDestination;
-    private readonly string newDestination;
-    private readonly Action<string, string> notifyAct;
-    private readonly Action<string, string> notifyRevert;
-
-    public MoveAction(string file, string toFolder, IFileSystem fileSystem,
-        Action<string, string> notifyAct = null, Action<string, string> notifyRevert = null)
+    public class MoveAction : IReversibleAction
     {
-        if (file == null) throw new ArgumentNullException(nameof(file));
-        if (toFolder == null) throw new ArgumentNullException(nameof(toFolder));
-        if (fileSystem == null) throw new ArgumentNullException(nameof(fileSystem));
-        if (!fileSystem.FileExists(file)) throw new FileNotFoundException(null, file);
-        if (!fileSystem.DirectoryExists(toFolder))
-            throw new DirectoryNotFoundException(
-                Text.DirectoryNotFoundExceptionMessage.Replace("{Directory}", toFolder,
-                    StringComparison.OrdinalIgnoreCase));
+        private readonly IFileSystem fileSystem;
+        private readonly string oldDestination;
+        private readonly string newDestination;
+        private readonly Action<string, string> notifyAct;
+        private readonly Action<string, string> notifyRevert;
 
-        this.fileSystem = fileSystem;
+        public MoveAction(string file, string toFolder, IFileSystem fileSystem,
+            Action<string, string> notifyAct = null, Action<string, string> notifyRevert = null)
+        {
+            if (file == null) throw new ArgumentNullException(nameof(file));
+            if (toFolder == null) throw new ArgumentNullException(nameof(toFolder));
+            if (fileSystem == null) throw new ArgumentNullException(nameof(fileSystem));
+            if (!fileSystem.FileExists(file)) throw new FileNotFoundException(null, file);
+            if (!fileSystem.DirectoryExists(toFolder))
+                throw new DirectoryNotFoundException(
+                    Text.DirectoryNotFoundExceptionMessage.Replace("{Directory}", toFolder,
+                        StringComparison.OrdinalIgnoreCase));
 
-        this.notifyAct = notifyAct;
-        this.notifyRevert = notifyRevert;
+            this.fileSystem = fileSystem;
 
-        // ensure absolute paths, there are weird windows path limit bugs
-        file = Path.GetFullPath(file);
-        toFolder = Path.GetFullPath(toFolder);
+            this.notifyAct = notifyAct;
+            this.notifyRevert = notifyRevert;
 
-        oldDestination = file;
-        newDestination = Path.Combine(toFolder, Path.GetFileName(file));
-    }
+            // ensure absolute paths, there are weird windows path limit bugs
+            file = Path.GetFullPath(file);
+            toFolder = Path.GetFullPath(toFolder);
 
-    public string DisplayName => Text.MoveActionMessage
-        .Replace("{FileName}", Path.GetFileName(oldDestination), StringComparison.OrdinalIgnoreCase)
-        .Replace("{Directory}", Path.GetDirectoryName(newDestination), StringComparison.OrdinalIgnoreCase);
+            oldDestination = file;
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(file);
+            string fileExtension = Path.GetExtension(file);
+            string newFileName = $"{fileNameWithoutExtension}_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}{fileExtension}";
+            newDestination = Path.Combine(toFolder, newFileName);
+        }
 
-    public void Act()
-    {
-        fileSystem.Move(oldDestination, newDestination);
+        public string DisplayName => Text.MoveActionMessage
+            .Replace("{FileName}", Path.GetFileName(oldDestination), StringComparison.OrdinalIgnoreCase)
+            .Replace("{Directory}", Path.GetDirectoryName(newDestination), StringComparison.OrdinalIgnoreCase);
 
-        notifyAct?.Invoke(oldDestination, newDestination);
-    }
+        public void Act()
+        {
+            fileSystem.Move(oldDestination, newDestination);
 
-    public void Revert()
-    {
-        fileSystem.Move(newDestination, oldDestination);
+            notifyAct?.Invoke(oldDestination, newDestination);
+        }
 
-        notifyRevert?.Invoke(newDestination, oldDestination);
+        public void Revert()
+        {
+            fileSystem.Move(newDestination, oldDestination);
+
+            notifyRevert?.Invoke(newDestination, oldDestination);
+        }
     }
 }
